0af22adbccd075fbbba688543cd0846d2b18e37b
infinite223 <dawidszmigiel9@gmail.com>
2024-10-22 23:03:06 +0200

    some changes, add functions from firebase

---
+++ functions/index.js
@@ -0,0 +1,24 @@
+const functions = require("firebase-functions");
+// const cors = require("cors")({ origin: true });
+// // Funkcja zwracająca statyczne dane
+// exports.getData = functions.https.onRequest(
+//   { cors: [/localhost\.com$/, "http://localhost.com"] },
+//   (req, res) => {
+//     res.json({
+//       message: "Hello from Firebase Functions!",
+//       data: [1, 2, 3, 4, 5],
+//     });
+//   }
+// );
+
+const { onRequest } = require("firebase-functions/v2/https");
+
+exports.sayHello = onRequest(
+  { cors: [/localhost\.com$/, "http://localhost.com"] },
+  (req, res) => {
+    res.json({
+      message: "Hello from Firebase Functions!",
+      data: [1, 2, 3, 4, 5],
+    });
+  }
+);
--- src/modules/home/home-view.vue
+++ src/modules/home/home-view.vue
@@ -3,6 +3,10 @@ import { collection, doc, getDocs } from "firebase/firestore";
 import { db } from "./../../services/firebase.config";
 import { RouterLink } from "vue-router";
 import AppLogo from "../../components/app-logo.vue";
+import { onMounted, ref } from "vue";
+
+const message = ref("");
+
 const handleClick = async () => {
   const data = await getDocs(collection(db, "cars"));
 
@@ -10,6 +14,21 @@ const handleClick = async () => {
     console.log(doc.id, " => ", doc.data());
   });
 };
+
+onMounted(async () => {
+  try {
+    const response = await fetch(
+      "http://127.0.0.1:5001/car-service-c65d7/us-central1/sayHello"
+    );
+    if (!response.ok) {
+      throw new Error("Błąd podczas pobierania danych");
+    }
+    const data = await response.json();
+    message.value = data.message;
+  } catch (error) {
+    console.error("Wystąpił błąd:", error);
+  }
+});
 </script>
 
 <template>
@@ -19,6 +38,8 @@ const handleClick = async () => {
     <main>
       <RouterView />
     </main>
+
+    {{ message }}
   </div>
 </template>
 
